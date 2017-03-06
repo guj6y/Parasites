5c22a857f2a1338bd34b42362562b360d343a002
Nic Kappler <nguj6y@gmail.com>
2017-03-06 16:42:43 -0700

    Makes tex tables with regressions

--- Code/Dynamical-Study/processExperimentOutput.m
+++ Code/Dynamical-Study/processExperimentOutput.m
@@ -15,12 +15,10 @@ function [dataOut,ME] = processExperimentOutput(dataCell,param,varargin)
 
 %Requires function handles for calculating statistics and error bars.
 
-%figured out why it's name-value pairs.  varargin (2n-1) is the field
-%name of the parameter strucutre and varargin(2n) is the value.  duh!
-
 %checking out what is inside the param structure:
 
 ME = struct();
+%Check for x input values
 try
     xVals = param.xVals;
     nXVal = numel(xVals);
@@ -29,6 +27,7 @@ catch errXVals
     return
 end
 
+%Check if we are showing a plot
 try
     makePlot = param.makePlot;
 catch errMakePlot
@@ -36,6 +35,7 @@ catch errMakePlot
     makePlot = false;
 end
 
+%Check if we are saving a plot
 try
     savePlot = param.savePlot;
 catch errSavePlot
@@ -43,32 +43,50 @@ catch errSavePlot
     savePlot = false;
 end
 
+%Check if we are going to calculate a regression
+try 
+    if param.regressionParam.doRegression
+        doReg = true;
+        regFcn = param.regressionParam.cmd;
+        regOpts = param.regressionParam.regOpts;
+        saveReg = param.regressionParam.saveReg;
+    end
+catch
+    doReg = false;
+    saveReg = false;
+end
+
+%Check if we are going to save the data to make our plots elsewhere.
 try
     saveData = param.saveData;
+    filepath = param.dataParam.filePath;
+    
 catch errSaveData
     ME.errSaveData = errSaveData;
     saveData = false;
 end
 
+%Make sure we can calculate statistics
 if ~isa(param.statFcn,'function_handle')
-    errorMessage = 'You must save input function handle in param.statFun\n';
+    errorMessage = 'You must input function handle in param.statFun\n';
     error(errorMessage);
 end
 
+%Make sure we can calculate error bars
 try
+    
     errBarsFcn = param.errBars.errBarsFcn;
     errBarsParam = param.errBars.param;
     
     lNull = zeros(2,1);
     uNull = zeros(2,1);
-    yNull = zeros(2,1);
-    
-    
+    yNull = zeros(2,1); 
 catch errErrBars
     ME.errBars = errErrBars;
     errBars = false;
 end
 
+%Make sure we have plotting parameters
 try
     plotParam = param.plotParam;
     marksMatrix = plotParam.marksMatrix;
@@ -78,15 +96,29 @@ catch errPlotParam
     ME.errPlotParam = errPlotParam;
     makePlot = false;
     savePlot = false;
-    warning('No plot parameters supplied; no plots will be made or saved.\n')
+    warning('No plot parameters supplied (or something was missing); no plots will be made or saved.\n')
 end
 
+% Make sure we have the model codes input
 try
     modelCodes = param.modelCodes;
     [nModels,nTreatments] = size(modelCodes);
 catch errModelCodes
     ME.errModelCodes = errModelCodes;
     return
+end
+
+%Check if we are saving the regressions (Save it as a table of 16
+%regressions with some statistics.
+if saveReg
+    filename = sprintf('%s%sRegressions.textab',filepath,param.dataAnalyzed);
+    regFidTable = fopen(filename,'w');
+    fprintf(regFidTable,'\\begin{tabular}{c |c c| c c |c c |c}\n\\hline\n');
+    fprintf(regFidTable,'$\\beta_0$&$p$-value&\\$beta_1$&$p$-value&$\\beta_2$&$p$-value&Adj. $R^2$\\\\\n \\hline\n');
+    %Going to include a column to include quadratics just to see if they
+    %are worth including or not.  I choose quadratic vs. linear model based
+    %on Adjusted r-squared; if linear is chosen, the column for the
+    %quadratic term is left blank.
 end
 
 
@@ -122,10 +154,14 @@ AXW = fullfact([2,2]);
 %This subplot structure probably needs to be hard-coded.  that's how I'm
 %going to want it, anyway.
 yNull = zeros(2,1);
+yNullVec = cell(2,1);
+
 nullChoice = factor1(:,2)+1;
 ymax = 0;
 ymin = inf;
-for subplotChoice = 1:4;
+y = zeros(size(xVals));
+yData = zeros(size(dataCell{1}));
+for subplotChoice = 1:4
     
     if makePlot||savePlot
         subplot(2,2,subplotChoice);
@@ -136,7 +172,7 @@ for subplotChoice = 1:4;
     dataOut.upper(:,1,subplotChoice) = xVals;
     dataOut.lower(:,1,subplotChoice) = xVals;
     
-    for factor1Choice = 1:2;
+    for factor1Choice = 1:2
         for factor2Choice = 1:2
             count = count+1;
             if makePlot||savePlot
@@ -148,28 +184,30 @@ for subplotChoice = 1:4;
                 factor4(:,AXW(subplotChoice,2))&...
                 factor1(:,factor1Choice);
             if nargin >=3
-                y = param.statFcn(dataCell{pickYs},varargin{1}(:),param.statFcnOpt);
+                y0 = param.statFcn(dataCell{pickYs},varargin{1}(:),param.statFcnOpt);
             else
-                y = param.statFcn(dataCell{pickYs},param.statFcnOpt);
+                y0 = param.statFcn(dataCell{pickYs},param.statFcnOpt);
             end
             
             if nargin ==4
-                [ql, qu] = errBarsFcn(dataCell{pickYs},errBarsParam,varargin{2}{pickYs});
+                [ql0, qu0] = errBarsFcn(dataCell{pickYs},errBarsParam,varargin{2}{pickYs});
             else
-                [ql, qu] = errBarsFcn(dataCell{pickYs},errBarsParam);
+                [ql0, qu0] = errBarsFcn(dataCell{pickYs},errBarsParam);
             end
             
-            if numel(y) == nXVal;
+            if numel(y0) == nXVal
                 
-                lNull(nullChoice(pickYs)) = ql(1);
-                uNull(nullChoice(pickYs)) = qu(1);
-                yNull(nullChoice(pickYs)) = y(1);
-                
+                lNull(nullChoice(pickYs)) = ql0(1);
+                uNull(nullChoice(pickYs)) = qu0(1);
+                yNull(nullChoice(pickYs)) = y0(1);
+                y = y0;
+                ql = ql0;
+                qu = qu0;
             else
                 
-                ql = [lNull(nullChoice(pickYs)) ql];
-                qu = [uNull(nullChoice(pickYs)) qu];
-                y = [yNull(nullChoice(pickYs)) y];
+                ql = [lNull(nullChoice(pickYs)) ql0];
+                qu = [uNull(nullChoice(pickYs)) qu0];
+                y = [yNull(nullChoice(pickYs)) y0];
                 
             end
             
@@ -182,8 +220,70 @@ for subplotChoice = 1:4;
                 ymax = max([ymax,max(y+qu)]);
                 ymin = min([ymin,min(y-ql)]);
             end
+            %Are we doing the regression?
+            if doReg
+                %Need to get the f**king null model (0% parasites) with y.
+                yData0 = dataCell{pickYs};
+                nRow = length(yData0);
+                %Picking the right null model as before
+                if numel(yData0) == nXVal*nRow
+
+                    yNullVec{nullChoice(pickYs)} = yData0(:,1);
+                    yData(:) = yData0(:);
+                else
+                
+                    yData(:) = [yNullVec{nullChoice(pickYs)} yData0];
+                    
+                end
+                
+                %Repeat the xvalues as necessary
+                nRow = length(yData);
+                X = repmat(xVals,nRow,1);
+                %Do the regressions.  If we don't specify options just do
+                %this.  if we do, not going to compare the two options.
+                if numel(fieldnames(regOpts))==0
+                    linRegMdl = regFcn(X(:),yData(:));
+                    quadRegMdl = regFcn(X(:),yData(:),'PureQuadratic');
+                else
+                    regMdl = regFcn(X(:),ydata(:),regOpts);
+                end
+                
+                %If we are going to save the regressions...
+                if saveReg
+                    %pick the better regression
+                    
+                    %Multi row for each subplot
+                    if (factor1Choice*factor2Choice) == 1
+                        fprintf(regFidTable,'\\\\multirow{4}{*}{subplot %c}',64+subplotChoice);
+                    end
+                    if linRegMdl.Rsquared.Adjusted > quadRegMdl.Rsquared.Adjusted
+                        %We choose linear here
+                        b0 = linRegMdl.Coefficients{1,1};
+                        b1 = linRegMdl.Coefficients{2,1};
+                        pVal0 = linRegMdl.Coefficients{1,4};
+                        pVal1 = linRegMdl.Coefficients{2,4};
+                        fprintf(regFidTable,'&%.3f&%.3e&%.3f&%.3e&&&%.3f\\\\\n'...
+                            ,b0,pVal0,b1,pVal1,linRegMdl.Rsquared.Adjusted);
+                    else
+                        b0 = quadRegMdl.Coefficients{1,1};
+                        b1 = quadRegMdl.Coefficients{2,1};
+                        b2 = quadRegMdl.Coefficients{3,1};
+                        
+                        pVal0 = quadRegMdl.Coefficients{1,4};
+                        pVal1 = quadRegMdl.Coefficients{2,4};
+                        pVal2 = quadRegMdl.Coefficients{3,4};
+                        fprintf(regFidTable,'&%.3f&%.3e&%.3f&%.3e&%.3f&%.3e&%.3f\\\\\n'...
+                            ,b0,pVal0,b1,pVal1,b2,pVal2,...
+                            quadRegMdl.Rsquared.Adjusted);
+                    end
+                        
+                end
+            end
             grid on
         end
+    end
+    if saveReg
+        fprintf(regFidTable,'\\hline\n');
     end
 end
 
@@ -236,17 +336,15 @@ if makePlot||savePlot
 end
 
 if savePlot
-    filename = param.plotParam.filename;
+    figFilename = param.plotParam.filename;
     format = param.plotParam.format;
-    
-    saveas(fig,filename,format)
+    saveas(fig,figFilename,format)
 end
 
 if saveData
     headers = param.dataParam.headers;
     for condition = 1:4
-        filepath = param.dataParam.filePath;
-
+        
         filename = sprintf('%s%s%s',filepath,param.dataAnalyzed);
         
         fid = fopen(sprintf('%s-small-small-subplot-%u',filename,condition),'w');
@@ -286,5 +384,5 @@ if saveData
         fclose(fid);
         
     end
-    
+    fprintf(regFidTable,'\\hline\n\\end{tabular}');
 end
